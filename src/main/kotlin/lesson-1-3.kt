26ed23ea44eb64438c9d227fcae4ca45d1a28b20
marusa <redblack23422@gmail.com>
2025-03-01 22:25:42 +0300

    данные о полете гагарина в космос

---
+++ src/main/kotlin/lesson-1-3.kt
@@ -0,0 +1,14 @@
+package org.example
+
+fun lesson13()
+{
+    val year: String = "1961"
+    var hour: String  = "09"      //взлет
+    var minute: String  = "07"
+    println(year)
+    println(hour)
+    println(minute)
+    hour = "10"  //посадка
+    minute = "55"
+    print("$hour:$minute")
+}
